ee7faf2265bbfec2ade37adce6bf5d51045b4e8f
DragonWarrior0619 <alexsandrpapob@gmail.com>
2020-01-11 03:44:12 +1000

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
+
+export class GoogleMapApp extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stores: [{lat: 44.8167302, lng: 20.3947898},
+               {latitude: 44.7167302, longitude: 20.2947898},
+               {latitude: 44.6167302, longitude: 20.1947898} ]
+    }
+  }
+
+  displayMarkers = () => {
+    return this.state.stores.map((store, index) => {
+      return <Marker key={index} id={index} position={{
+        lat: store.latitude,
+        lng: store.longitude
+      }}
+      onClick={() => console.log("You clicked me!")}/>
+    })
+  }
+
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={32}
+        style={mapStyles}
+        initialCenter={{ lat: 44.50, lng: 20.41}}
+      >
+        {this.displayMarkers()}
+      </Map>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'Your API Key'
+})(GoogleMapApp);
